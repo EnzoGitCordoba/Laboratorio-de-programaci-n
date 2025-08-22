febfbf1fe1fb4c86db45de4a2c03e15ac3f9db62
enzos <enzo.sanmiguel@est.fi.uncoma.edu.ar>
2025-08-22 17:50:04 -0300

    Intento agregar la parte de fork join a la jugueteria , falta recursive task

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
                 .build();
         System.out.println("Juguete creado: " + juguete.toString());
 
-        Juguete juguete1 = new Juguete.Builder("Matuqui", 8000)
+        Juguete juguete1 = new Juguete.Builder("Matuqui", 808880)
                 .Dureza(10.0)
                 .build();
         System.out.println("Juguete creado: " + juguete1.toString());
@@ -27,7 +27,7 @@ public class Main {
                 .build();
         System.out.println("Juguete creado: " + juguete2.toString());
 
-        Juguete juguete3 = new Juguete.Builder("Pelota", 5000)
+        Juguete juguete3 = new Juguete.Builder("Pelota", 555000)
 
                 .Dureza(2)
                 .build();
@@ -47,7 +47,31 @@ public class Main {
                 .build();
         System.out.println("Juguete creado: " + juguete5.toString());
 
+        double [] arreglo= new double[6];
+        arreglo[0]=juguete.getPrecio();
+        arreglo[1]=juguete1.getPrecio();
+        arreglo[2]=juguete2.getPrecio();
+        arreglo[3]=juguete3.getPrecio();
+        arreglo[4]=juguete4.getPrecio();
+        arreglo[5]=juguete5.getPrecio();
+        // meto todos los precios de juguetes en un arreglo , para luego compararlos
 
+        Hilo h1= new Hilo(arreglo,0,2); // el constructor del hilo recibe el arreglo , y que distancia debe recorrer
+        Hilo h2= new Hilo(arreglo,2,4);
+        Hilo h3= new Hilo(arreglo,4,6);
+
+        h1.start(); //arranca el hilo
+        h2.start();
+        h3.start();
+
+        try {
+            h3.sleep(1500); //el hilo 3 espera un poco , para asegurar la finalizacion de los otros hilos
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("el maximo general es de " + h3.getMaxGlobal() ); //solicito el valor de la variable global de la clase HILO
 
     }
+
 }
