86ef34749554833a327273e37e2c885b67bcda35
enzos <enzo.sanmiguel@est.fi.uncoma.edu.ar>
2025-08-28 19:15:26 -0300

    Se le añadieron souts al pool de hilos para identificar como recorren el arreglo

--- MayorTask.java
+++ MayorTask.java
@@ -23,7 +23,7 @@ public class MayorTask extends RecursiveTask<Double> {
 
         if((fin-inicio)<=5){                            // cada "hilo" debera preguntar si su tramo de arreglo es menor a 5
             double maxLocal = arreglo[inicio];             // si lo es , creamos un maxLocal con la posicion inicial que va a comparar
-
+            System.out.println("Soy un hilo que recorre desde:"+inicio+" hasta:"+fin+" mi mayor local es:"+maxLocal);
             for (int i = inicio; i < fin; i++) {        // recorro todo el arreglito para ver el maxLocal
                 if (arreglo[i] > maxLocal) {
                     maxLocal = arreglo[i];          // Guardo el maxLocal
@@ -32,7 +32,8 @@ public class MayorTask extends RecursiveTask<Double> {
             }
             resultado=maxLocal;                 //como resultado es lo que voy a retornar , maxLocal= resultado
         }else{
-                                                    //en el caso de que no haya sido el tramo menor a 5 entraremos a bifurcar la tarea
+            System.out.println("Soy un hilo y intento recorrer desde: "+inicio+" hasta:"+fin+"pero como es muy grande divido la tarea");
+            //en el caso de que no haya sido el tramo menor a 5 entraremos a bifurcar la tarea
             int medio = (inicio+fin)  /2 ;      //dividimos el arreglo en 2
             MayorTask izquierda=new MayorTask(arreglo,inicio,medio);        //Creamos dos "hilos" los cuales se dividiran la tarea
             MayorTask derecha=new MayorTask(arreglo,medio,fin);
@@ -42,7 +43,7 @@ public class MayorTask extends RecursiveTask<Double> {
 
             resultadoDerecha = derecha.compute();// llama recursivamente a compute con el lado derecho del arreglo y almacena el valor retornado (resultado=MaxLocal)
             resultadoIzquierda=izquierda.join();       //espera a que termine la tarea izquierda y guarda el valor
-
+            System.out.println("Mis subHilos, me devolvieron dos valores. ResultadoIzquierda="+resultadoIzquierda+" y ResultadoDerecha"+resultadoDerecha);
             resultado =Math.max(resultadoDerecha,resultadoIzquierda);       // por cada bifurcacion resultante vamos a comparar y devulver un rexzultado
 
         }
